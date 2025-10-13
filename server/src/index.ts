207c8d8644c56a2c706e90162dbf3c4e2dca5385
leoraj0405 <leo006401@gmail.com>
2025-10-13 18:48:08 +0530

    feat: department endpoint implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ dotenv.config();
 import adminController from './router/admin/admin.controller';
 import managementController from './router/mangement/management.controller';
 import staffController from './router/staff/staff.controller';
+import departmentController from './router/department/department.controller';
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3200;
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/admin/', adminController)
 app.use('/api/management/', managementController)
 app.use('/api/staff/', staffController)
+app.use('/api/department/', departmentController);
 
 
 app.listen(PORT, () => {
